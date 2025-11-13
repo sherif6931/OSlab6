fa682406a61a1383034826eb57c8a52583afc0f1
sherif6931 <sherif6931@github.com>
2025-11-13 04:19:11 +0200

    proto/test file

---
+++ src/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+pid_t child2_pid;
+
+void sigchld_handler(int sig) {
+    int status;
+    pid_t pid = wait(&status);
+
+    if (pid == -1) return;
+
+    if (pid != child2_pid) {
+        int exit_code = WEXITSTATUS(status);
+        if (exit_code == 1) {
+            kill(child2_pid, SIGKILL);
+        } else {
+            kill(child2_pid, SIGCONT);
+        }
+    }
+}
+
+int main() {
+    signal(SIGCHLD, sigchld_handler);
+
+    pid_t child1_pid = fork();
+    if (child1_pid == 0) {
+        printf("Child1 PID: %d Parent PID: %d\n", getpid(), getppid());
+        sleep(1);
+        int value;
+        scanf("%d", &value);
+        exit(value);
+    }
+
+    child2_pid = fork();
+    if (child2_pid == 0) {
+        printf("Child2 PID: %d Parent PID: %d\n", getpid(), getppid());
+        kill(getpid(), SIGSTOP);
+        printf("child process is resumed\n");
+        exit(0);
+    }
+
+    while(1) {
+        pause();
+    }
+
+    return 0;
+}
